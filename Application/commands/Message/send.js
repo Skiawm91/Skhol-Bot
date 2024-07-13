581770eaa8fde1a410ea445b83cf436b289757fd
Skiawm91 <guoy2674@outlook.com>
2024-07-13 15:59:52 +0800

    現在 /send 指令可以發送嵌入式訊息 管理指令執行失敗現在會提示錯誤

--- Application/commands/Message/send.js
+++ Application/commands/Message/send.js
@@ -14,11 +14,29 @@ module.exports = {
             option
                 .setName('訊息')
                 .setDescription('輸入你想發送的訊息')
-                .setRequired(true)),
+                .setRequired(true))
+        .addBooleanOption((option) =>
+            option
+                .setName('嵌入式')
+                .setDescription('使用嵌入式訊息')
+                .setRequired(true))
+        .addStringOption((option) =>
+            option
+                .setName('標題')
+                .setDescription('嵌入式標題'))
+        .addStringOption((option) =>
+            option
+                .setName('內文')
+                .setDescription('嵌入式內文')),
     async execute(interaction) {
         const channel = interaction.options.getChannel('頻道');
         const message = interaction.options.getString('訊息');
-        channel.send(message);
+        if (interaction.options.getBoolean('嵌入式') == true) {
+            var Embed = new EmbedBuilder()
+                .setTitle(interaction.options.getString('標題'))
+                .setDescription(interaction.options.getString('內文'));
+        }
+        channel.send({ content: message, embeds: [Embed] });
         await interaction.reply({ content: '已成功發送訊息！', ephemeral: true })
     },
 };
