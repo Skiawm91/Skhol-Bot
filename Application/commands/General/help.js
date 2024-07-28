a3c70a26e6ccbe308e4c688d0fe4e4fe777201d6
Skiawm91 <guoy2674@outlook.com>
2024-07-28 12:24:03 +0800

    0.2.8

--- Application/commands/General/help.js
+++ Application/commands/General/help.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(interaction) {
         const Embed = new EmbedBuilder()
             .setAuthor({ name: 'Skhol Bot', iconURL: 'https://skiawm91.github.io/Sakura_Inc/Skhol_Bot/Avatar.png', url: 'https://skiawm91.github.io/Sakura_Inc/Skhol_Bot' })
-            .setDescription(`歡迎使用 **Skhol Bot**！這裡是指令列表。\n在聊天欄輸入斜線 `/` 即可使用指令！\n選擇一個指令類別來取得指令詳細訊息！\n\n官方DC群組: [點擊加入！](https://discord.gg/CWM8zvs7ht)`)
+            .setDescription('歡迎使用 **Skhol Bot**！這裡是指令列表。\n在聊天欄輸入斜線 `/` 即可使用指令！\n選擇一個指令類別來取得指令詳細訊息！\n\n官方DC群組: [點擊加入！](https://discord.gg/CWM8zvs7ht)')
             .setFooter({ text: 'Made By Skiawm91' });
         const Select = new StringSelectMenuBuilder()
             .setCustomId('選擇指令類別')
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Skhol Bot
 **一個開源的應用程式（ Discord 這樣稱呼 ）
 <br/><br/>
-Code: `0.2.7`
+Code: `0.2.8`
 <br/>
-Release: `0.2.6`**
+Release: `0.2.7`**
 ## 需求
 1. **[Node.js v18 及以上版本](https://nodejs.org/en/download/prebuilt-installer)**
 ## 如何使用 `Windows/macOS/Linux`
